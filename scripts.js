f155e68907609ecd991ec04b8de7230d0fe7966f
luigi-apis3 <luigi.fidencio@apis3.com>
2024-11-07 14:37:03 -0300

    att

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navbar = document.querySelector(".navbar.sidebar");
+
+    hamburger.addEventListener("click", function () {
+        hamburger.classList.toggle("active"); // Alterna entre ☰ e ✖
+        navbar.classList.toggle("active");     // Alterna a exibição do menu
+    });
+});
+
 document.querySelectorAll("a[href^='#']").forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
         e.preventDefault();
